34ef7b8f726792f9de7b7a571f844d55387b824c
Shirashoji <tkm.shira@gmail.com>
2019-03-03 10:01:04 +0900

    いらない情報を削除

--- click.py
+++ click.py
@@ -1,10 +1,10 @@
 import pyautogui as ag
 import time
-#coordinate 座標
+
+
 clicktimes = int(input('何回繰り返しますか: '))
-#clicktimes = 100
+
 time.sleep(5)
 
 for i in range(clicktimes):
     ag.click()
-    #ag.typewrite('Hello world!\n')  # intervalは文字間の入力待機時間です．
--- linebot2.py
+++ linebot2.py
@@ -7,7 +7,7 @@ import pyperclip
 text = input("何を送りますか?: ")
 pyperclip.copy(text)
 
-#coordinate 座標
+
 clicktimes = int(input('何回繰り返しますか: '))
 
 time.sleep(5)
@@ -17,7 +17,6 @@ for i in range(clicktimes):
     pyautogui.hotkey('command','v') #Mac
 #    pyautogui.hotkey('ctrl','v') #Win
     pyautogui.hotkey('command','\n')
-#    time.sleep(0.00001)
 
 
 pyautogui.typewrite('Written by Python Program.')
